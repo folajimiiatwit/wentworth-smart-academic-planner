962ec03a1f1ddd5d61f71e670910514397e9af89
folajimiiatwit <144471241+folajimiiatwit@users.noreply.github.com>
2026-05-29 19:03:04 -0400

    Enhance login_user to handle new usernames
    Trim username input and handle new users during login.

--- backend/auth.py
+++ backend/auth.py
@@ -1,3 +1,24 @@
 import pandas as pd
 from backend.data_manager import load_users, save_users, ELECTIVE_COLUMNS
 def login_user(username):
+  username = username.strip().lower()
+  if username=="":
+    return {"error": "invalid username"}
+  users=load_users()
+  existing_usernames=users["username"].astype(str).str.lower().values
+
+  if username not in existing_usernames:
+    new_user_data = {
+      "username":username, "completed_required_courses":"",
+      "planned_courses": ""
+    }
+
+    for column in ELECTIVE_COLUMNS:
+      new_user_data[column]=0
+
+    new_user = pd.DataFrame([new_user_data])
+    users=pd.concat([users, new_user], ignore_index=True)
+    save_users(users)
+    
+  return{"message":"Login Successful", "username": username}
+    
