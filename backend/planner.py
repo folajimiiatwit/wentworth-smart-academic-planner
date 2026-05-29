c2fbf0a1d511e704afaebe482a1cbd16268e30ba
folajimiiatwit <144471241+folajimiiatwit@users.noreply.github.com>
2026-05-29 12:00:20 -0400

    Add elective requirements constants to planner.py

--- backend/planner.py
+++ backend/planner.py
@@ -1,1 +1,12 @@
+from datetime import datetime
+import re
 
+ELECTIVE_REQUIREMENTS = {
+    "general_elective_credits": 8,
+    "cs_elective_credits": 16,
+    "english_elective_credits": 8,
+    "science_elective_credits": 8,
+    "humanities_elective_credits": 4,
+    "social_science_elective_credits": 4,
+    "ethics_elective_credits": 4
+}
