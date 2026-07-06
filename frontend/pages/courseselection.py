9b497926af1bc51175a743595106136cafd72676
folajimiiatwit <folajimii@wit.edu>
2026-07-06 15:27:05 -0400

    added course selection tabs for better visibility

--- frontend/pages/courseselection.py
+++ frontend/pages/courseselection.py
@@ -23,7 +23,50 @@ data.ensure_backend_running()
 ui.set_png_as_page_bg(BASE_DIR/ "assets/background.jpg") 
 ui.page_config()
 ui.render_header()
+st.title("Course Selection")
+st.caption("Manage completed courses, elective credits, and degree progress.")
 
+
+elective_saved = load_user_data()
+
+tab1, tab2, tab3, tab4 = st.tabs([
+    "Transcript",
+    "Completed Courses",
+    "Progress",
+    "AI Plan"
+])
+
+with tab1:
+    st.subheader("Transcript Upload")
+    #Way to upload files here
+
+    st.divider()
+
+    st.subheader("Add Course Manually")
+    #Way to add courses manually here (preferable by typing course name (eg. COMP), course number, and title(optional))
+
+with tab2:
+    #show result of transcript analysis 
+
+    st.divider()
+
+    #save results
+with tab3:
+    #see current progress
+    st.divider()
+
+with tab4:
+    #button to generate curiculum map
+    st.divider()
+
+st.divider()
+
+bottom_left, bottom_right = st.columns([4, 1])
+
+#move to calandar page
+with bottom_right:
+    if st.button("Schedule Builder", width="stretch"):
+        st.switch_page("pages/schedule.py")
 #* note to self add a view of the calendar and split it in half such that there's the ability to pick classes and stuff while watching in real time 
 # page flow will be 
 
